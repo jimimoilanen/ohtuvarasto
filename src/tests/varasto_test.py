c1bd312c9c1102973439e7d736bbcce5f7bb0adb
Jimi Moilanen <jimi.p.moilanen@student.jyu.fi>
2025-11-03 19:55:24 +0200

    testit täydennetty 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,8 +10,18 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_alku_saldo_negatiivinen(self):
+        self.varasto = Varasto(10, -2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    def test_negatiivinen_tilavuus(self):
+        self.varasto = Varasto(-2)
+        
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
@@ -24,12 +34,37 @@ class TestVarasto(unittest.TestCase):
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
+    def test_lisays_lisataan_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_lisays_lisataan_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(100)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
 
         saatu_maara = self.varasto.ota_varastosta(2)
 
         self.assertAlmostEqual(saatu_maara, 2)
+
+    def test_ottaminen_otetaan_negatiivinen(self):
+        saatu_maara = self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_ottaminen_otetaan_enemmän(self):
+        self.varasto.lisaa_varastoon(5)
+
+        saatu_maara = self.varasto.ota_varastosta(100)
+
+        self.assertAlmostEqual(saatu_maara, 5)
+
+    def test_string(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
